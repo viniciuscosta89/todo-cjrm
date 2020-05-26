5721f771cd4d1c35d2321e967558bc986b4ae8af
Vinicius Costa <vinicius.costa89@gmail.com>
2020-05-26 18:44:20 -0300

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 const formAddTodo = document.querySelector('.form-add-todo')
+const inputSearchTodo = document.querySelector('.form-search input')
+const todosContainer = document.querySelector('.todos-container')
+
+formAddTodo.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const inputValue = event.target.add.value.trim()
+
+  if (inputValue.length) {
+    todosContainer.innerHTML += `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${inputValue}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+    `
+
+    event.target.reset()
+  }
+})
+
+todosContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+  const clickedElementClassesArray = Array.from(clickedElement.classList)
+
+  if (clickedElementClassesArray.includes('delete')) {
+    clickedElement.parentElement.remove()
+  }
+})
+
+inputSearchTodo.addEventListener('input', e => {
+  const inputValue = e.target.value.trim().toLowerCase()
+
+  Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('d-flex')
+      todo.classList.add('hidden')
+    })
+
+  Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+    .forEach(todo => {
+      todo.classList.remove('hidden')
+      todo.classList.add('d-flex')
+    })
+})
